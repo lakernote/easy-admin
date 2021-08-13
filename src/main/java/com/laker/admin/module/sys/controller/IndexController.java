0bbbd172d1508cba01d9675db141afe82854801c
laker <935009066@qq.com>
2021-08-13 17:29:42 +0800

    (新增)[后端](引入流程引擎)

--- src/main/java/com/laker/admin/module/sys/controller/IndexController.java
+++ src/main/java/com/laker/admin/module/sys/controller/IndexController.java
@@ -1,16 +1,31 @@
 package com.laker.admin.module.sys.controller;
 
+import cn.hutool.captcha.CaptchaUtil;
+import cn.hutool.captcha.LineCaptcha;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * /admin/** 无需check login
+ */
 @Controller
-@RequestMapping("/admin")
+@Slf4j
 public class IndexController {
 
-    @GetMapping({"", "/index", "/"})
+    @GetMapping({"/admin", "/admin/index", "/admin/"})
     public String adminIndex() {
         return "redirect:/admin/index.html";
     }
 
+
+    @GetMapping("/captcha")
+    public void captcha(HttpServletResponse response) throws IOException {
+        LineCaptcha captcha = CaptchaUtil.createLineCaptcha(100, 40, 4, 2);
+        log.info("当前验证码：{}", captcha.getCode());
+        captcha.write(response.getOutputStream());
+    }
 }
