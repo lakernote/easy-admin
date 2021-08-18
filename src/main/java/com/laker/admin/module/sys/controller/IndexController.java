771d2b685a777a46171900ac27ef0bf5c512a519
laker <935009066@qq.com>
2021-08-18 14:20:01 +0800

    (新增)[整体](增加定时任务管理)

--- src/main/java/com/laker/admin/module/sys/controller/IndexController.java
+++ src/main/java/com/laker/admin/module/sys/controller/IndexController.java
@@ -4,7 +4,8 @@ import cn.hutool.core.lang.Dict;
 import cn.hutool.core.util.IdUtil;
 import com.laker.admin.framework.Response;
 import com.laker.admin.framework.cache.ICache;
-import com.wf.captcha.SpecCaptcha;
+import com.wf.captcha.ArithmeticCaptcha;
+import com.wf.captcha.ChineseGifCaptcha;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -33,14 +34,21 @@ public class IndexController {
     @GetMapping("/captcha")
     @ResponseBody
     public Response captcha() {
-        SpecCaptcha specCaptcha = new SpecCaptcha(130, 48, 5);
-        String verCode = specCaptcha.text().toLowerCase();
+//        SpecCaptcha specCaptcha = new SpecCaptcha(130, 48, 5);
+        // 中文gif类型
+//        ChineseGifCaptcha specCaptcha = new ChineseGifCaptcha(130, 48, 4);
+        ArithmeticCaptcha captcha = new ArithmeticCaptcha(130, 48);
+        // 几位数运算，默认是两位
+        captcha.setLen(3);
+        captcha.getArithmeticString();  // 获取运算的公式：3+2=?
+        captcha.text();  // 获取运算的结果：5
+        String verCode = captcha.text().toLowerCase();
         String uid = IdUtil.simpleUUID();
         log.info("当前uid:{},验证码：{}", uid, verCode);
         // 前后端分离，这时还未有会话信息，用于确定uid和验证码的一一映射关系，防止多人串码
         // 把uuid和图片码一起给前端，验证的时候再一起回来
         iCache.put(uid, verCode, 3 * 60);
 
-        return Response.ok(Dict.create().set("uid", uid).set("image", specCaptcha.toBase64()));
+        return Response.ok(Dict.create().set("uid", uid).set("image", captcha.toBase64()));
     }
 }
