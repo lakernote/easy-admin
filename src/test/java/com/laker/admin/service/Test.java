c01928c32ade8e4e1a97fdd1263c6ee149e666e7
laker <935009066@qq.com>
2021-09-22 15:46:03 +0800

    (update)[整体](测试BCrypt摘要加密)

--- src/test/java/com/laker/admin/service/Test.java
+++ src/test/java/com/laker/admin/service/Test.java
@@ -2,6 +2,7 @@ package com.laker.admin.service;
 
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.crypto.SecureUtil;
+import cn.hutool.crypto.digest.BCrypt;
 import com.laker.admin.utils.IP2CityUtil;
 
 public class Test {
@@ -9,7 +10,10 @@ public class Test {
     public static void main(String[] args) {
         String s = SecureUtil.sha256("lakernote");
         System.out.println(s);
-
+        String pw_hash = BCrypt.hashpw("lakernote", BCrypt.gensalt());
+        System.out.println("密文：" + pw_hash);
+        boolean checkpw = BCrypt.checkpw("lakernote", pw_hash);
+        System.out.println("校验结果：" + checkpw);
         String cityInfo = IP2CityUtil.getCityInfo("120.36.3.41");
         System.out.println(cityInfo);
         String[] split = cityInfo.split("\\|");
