5d49fb8e15db6bb6642abf97b2a28731c0f54fa7
laker <935009066@qq.com>
2021-10-12 10:35:15 +0800

    (add)[整体](修改流程设计模块增加支持流程固定人员选择以及流程xml在线编辑)

--- src/main/java/com/laker/admin/framework/ext/mybatis/UserInfoAndPowers.java
+++ src/main/java/com/laker/admin/framework/ext/mybatis/UserInfoAndPowers.java
@@ -4,6 +4,7 @@ import com.laker.admin.module.enums.DataFilterTypeEnum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Map;
 import java.util.Set;
@@ -11,6 +12,7 @@ import java.util.Set;
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserInfoAndPowers {
     DataFilterTypeEnum filterType = DataFilterTypeEnum.SELF;
     private Long userId;
