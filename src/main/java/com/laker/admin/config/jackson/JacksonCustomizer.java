ee53c296c756292101166c0b85f9a8274bd6f835
laker <935009066@qq.com>
2022-09-15 14:05:28 +0800

    解决ONLY_FULL_GROUP_BY报错问题

--- src/main/java/com/laker/admin/config/jackson/JacksonCustomizer.java
+++ src/main/java/com/laker/admin/config/jackson/JacksonCustomizer.java
@@ -1,6 +1,8 @@
 package com.laker.admin.config.jackson;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
@@ -34,25 +36,25 @@ public class JacksonCustomizer implements Jackson2ObjectMapperBuilderCustomizer 
 
         //处理LocalDateTime
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter
-            .ofPattern(STANDARD_PATTERN);
-        javaTimeModule.addSerializer(LocalDateTime.class, 
-                                     new LocalDateTimeSerializer(dateTimeFormatter));
-        javaTimeModule.addDeserializer(LocalDateTime.class, 
-                                       new LocalDateTimeDeserializer(dateTimeFormatter));
+                .ofPattern(STANDARD_PATTERN);
+        javaTimeModule.addSerializer(LocalDateTime.class,
+                new LocalDateTimeSerializer(dateTimeFormatter));
+        javaTimeModule.addDeserializer(LocalDateTime.class,
+                new LocalDateTimeDeserializer(dateTimeFormatter));
 
         //处理LocalDate
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
-        javaTimeModule.addSerializer(LocalDate.class, 
-                                     new LocalDateSerializer(dateFormatter));
-        javaTimeModule.addDeserializer(LocalDate.class, 
-                                       new LocalDateDeserializer(dateFormatter));
+        javaTimeModule.addSerializer(LocalDate.class,
+                new LocalDateSerializer(dateFormatter));
+        javaTimeModule.addDeserializer(LocalDate.class,
+                new LocalDateDeserializer(dateFormatter));
 
         //处理LocalTime
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern(TIME_PATTERN);
-        javaTimeModule.addSerializer(LocalTime.class, 
-                                     new LocalTimeSerializer(timeFormatter));
-        javaTimeModule.addDeserializer(LocalTime.class, 
-                                       new LocalTimeDeserializer(timeFormatter));
+        javaTimeModule.addSerializer(LocalTime.class,
+                new LocalTimeSerializer(timeFormatter));
+        javaTimeModule.addDeserializer(LocalTime.class,
+                new LocalTimeDeserializer(timeFormatter));
 
         /*
          * 1. java.util.Date yyyy-MM-dd HH:mm:ss
@@ -61,11 +63,15 @@ public class JacksonCustomizer implements Jackson2ObjectMapperBuilderCustomizer 
          * 4. 序列化时包含所有字段
          * 5. 在序列化一个空对象时时不抛出异常
          * 6. 忽略反序列化时在json字符串中存在, 但在java对象中不存在的属性
+         * 7. 序列化：日期/时间是否序列化为时间戳，这里禁止
+         * 8. 允许忽略未知枚举值和通过@JsonEnumDefaultValue注释指定的预定义值的功能。如果禁用，未知的枚举值将引发异常。如果启用，但未指定预定义的默认 Enum 值，也会引发异常。
          */
         builder.simpleDateFormat(STANDARD_PATTERN)
                 .modules(javaTimeModule, new Jdk8Module())
                 .serializationInclusion(JsonInclude.Include.ALWAYS)
                 .failOnEmptyBeans(false)
-                .failOnUnknownProperties(false);
+                .failOnUnknownProperties(false)
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .featuresToEnable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE);
     }
 }
