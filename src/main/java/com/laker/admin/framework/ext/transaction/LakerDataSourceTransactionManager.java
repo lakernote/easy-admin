4a628126e796dcc993b75e42565859c62d429d12
laker <935009066@qq.com>
2022-10-18 18:18:15 +0800

    增加长事务监控

--- src/main/java/com/laker/admin/framework/ext/transaction/LakerDataSourceTransactionManager.java
+++ src/main/java/com/laker/admin/framework/ext/transaction/LakerDataSourceTransactionManager.java
@@ -3,10 +3,13 @@ package com.laker.admin.framework.ext.transaction;
 import com.laker.admin.framework.aop.trace.SpanType;
 import com.laker.admin.framework.aop.trace.TraceContext;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 
 import javax.sql.DataSource;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author: laker
@@ -15,27 +18,46 @@ import javax.sql.DataSource;
 @Slf4j
 public class LakerDataSourceTransactionManager extends DataSourceTransactionManager {
 
-    transient ThreadLocal<Transaction> dateThreadLocal = new ThreadLocal<>();
 
-    public LakerDataSourceTransactionManager(DataSource dataSource) {
+    transient ThreadLocal<LinkedList<Transaction>> dateThreadLocal = new ThreadLocal<>();
+    private transient long costTime;
+
+    public LakerDataSourceTransactionManager(DataSource dataSource, long costTime) {
         super(dataSource);
+        this.costTime = costTime;
     }
 
     @Override
     protected void doBegin(Object transaction, TransactionDefinition definition) {
         String name = definition.getName();
         TraceContext.addSpan("LakerDataSourceTransactionManager.doBegin", SpanType.Transaction);
-        dateThreadLocal.set(new Transaction(System.currentTimeMillis(), name));
+        List<Transaction> transactionList = dateThreadLocal.get();
+        // init
+        if (CollectionUtils.isEmpty(transactionList)) {
+            LinkedList<Transaction> linkedList = new LinkedList<>();
+            linkedList.add(new Transaction(System.currentTimeMillis(), name));
+            dateThreadLocal.set(linkedList);
+        } else {
+            transactionList.add(new Transaction(System.currentTimeMillis(), name));
+        }
         super.doBegin(transaction, definition);
     }
 
     @Override
     protected void doCleanupAfterCompletion(Object transaction) {
+        LinkedList<Transaction> transactions = dateThreadLocal.get();
+        if (CollectionUtils.size(transactions) > 1) {
+            transactions.removeLast();
+        } else {
+            Transaction transaction1 = transactions.get(0);
+            long cost = System.currentTimeMillis() - transaction1.begin;
+            dateThreadLocal.remove();
+            if (cost > costTime) {
+                log.info("事务耗时监控！transaction:{},time:{}ms", transaction1.name, cost);
+            }
+        }
         super.doCleanupAfterCompletion(transaction);
-        TraceContext.stopSpan();
-        Transaction transactionT = dateThreadLocal.get();
-        log.info("事务耗时监控！transaction:{},time:{}ms", transactionT.name, System.currentTimeMillis() - transactionT.begin);
-        dateThreadLocal.remove();
+        TraceContext.stopSpan(costTime);
     }
 
     class Transaction {
