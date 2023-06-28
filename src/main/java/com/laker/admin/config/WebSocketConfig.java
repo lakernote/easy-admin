19b41bc866515c5479069996a02edcf718843058
laker <935009066@qq.com>
2023-06-28 09:30:09 +0800

    提交websocket代码示例

---
+++ src/main/java/com/laker/admin/config/WebSocketConfig.java
@@ -0,0 +1,44 @@
+package com.laker.admin.config;
+
+import com.laker.admin.framework.ext.websocket.LakerSessionHandshakeInterceptor;
+import com.laker.admin.framework.ext.websocket.MyHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+
+@Configuration
+@EnableWebSocket // 启动Websocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myHandler(), "/websocket/**")
+            // 添加拦截器，可以获取连接的param和 header 用作认证鉴权
+            .addInterceptors(new LakerSessionHandshakeInterceptor())
+            // 设置运行跨域
+            .setAllowedOrigins("*");
+    }
+       
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        // 设置默认会话空闲超时 以毫秒为单位 非正值意味着无限超时，默认值 0 ，默认没10s检查一次空闲就关闭
+        container.setMaxSessionIdleTimeout(10 * 1000L);
+        // 设置异步发送消息的默认超时时间 以毫秒为单位 非正值意味着无限超时 ，默认值-1，还没看到作用
+//        container.setAsyncSendTimeout(10 * 1000L);
+        // 设置文本消息的默认最大缓冲区大小 以字符为单位，默认值 8 * 1024
+        container.setMaxTextMessageBufferSize(8 * 1024);
+        // 设置二进制消息的默认最大缓冲区大小 以字节为单位，默认值 8 * 1024
+        container.setMaxBinaryMessageBufferSize(8 * 1024);
+        return container;
+    }
+
+    @Bean
+    public WebSocketHandler myHandler() {
+        return new MyHandler();
+    }
+
+}
---
+++ src/main/java/com/laker/admin/framework/ext/websocket/MyHandler.java
@@ -0,0 +1,88 @@
+package com.laker.admin.framework.ext.websocket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.socket.*;
+import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+import org.springframework.web.socket.handler.ConcurrentWebSocketSessionDecorator;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+public class MyHandler extends AbstractWebSocketHandler {
+    private final Map<String, WebSocketSession> webSocketSessionMap = new ConcurrentHashMap<>();
+
+    //成功连接时
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+        // 设置最大报文大小，如果报文过大则会报错的,可以将限制调大。
+        // 会覆盖config中的配置。
+        session.setBinaryMessageSizeLimit(8 * 1024);
+        session.setTextMessageSizeLimit(8 * 1024);
+        webSocketSessionMap.put(session.getId(), new ConcurrentWebSocketSessionDecorator(session, 10 * 1000, 64000));
+    }
+
+    //处理textmessage
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+        // 有消息就广播下
+        for (Map.Entry<String, WebSocketSession> entry : webSocketSessionMap.entrySet()) {
+            String s = entry.getKey();
+            WebSocketSession webSocketSession = entry.getValue();
+            if (webSocketSession.isOpen()) {
+                webSocketSession.sendMessage(new TextMessage(s + ":" + message.getPayload()));
+                log.info("send to {} msg:{}", s, message.getPayload());
+            }
+        }
+    }
+
+
+    //报错时
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        super.handleTransportError(session, exception);
+        log.warn("handleTransportError:: sessionId: {} {}", session.getId(), exception.getMessage(), exception);
+        if (session.isOpen()) {
+            try {
+                session.close();
+            } catch (Exception ex) {
+                // ignore
+            }
+        }
+    }
+
+    //连接关闭时
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+        WebSocketSession concurrentSession = webSocketSessionMap.remove(session.getId());
+        if (concurrentSession.isOpen()) {
+            try {
+                concurrentSession.close();
+            } catch (Exception ex) {
+                // ignore
+            }
+        }
+    }
+
+    //处理binarymessage
+    @Override
+    protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws Exception {
+        super.handleBinaryMessage(session, message);
+    }
+
+    //处理pong
+    @Override
+    protected void handlePongMessage(WebSocketSession session, PongMessage message) throws Exception {
+        super.handlePongMessage(session, message);
+    }
+
+    //是否支持报文拆包
+    // 决定是否接受半包，因为websocket协议比较底层，好像Tcp协议一样，如果发送大消息可能会拆成多个小报文。如果不希望处理不完整的报文，希望底层帮忙聚合成完整消息将此方法返回false,这样底层会等待完整报文到达聚合后才回调。
+    @Override
+    public boolean supportsPartialMessages() {
+        return super.supportsPartialMessages();
+    }
+}
