e3463e13561a35520604d76c9196bedfd1cab983
laker <935009066@qq.com>
2024-06-14 14:23:04 +0800

    feature: add KafkaHealthIndicator

--- src/main/java/com/laker/admin/framework/kafka/KafkaHealthIndicator.java
+++ src/main/java/com/laker/admin/framework/kafka/KafkaHealthIndicator.java
@@ -1,8 +1,10 @@
 package com.laker.admin.framework.kafka;
 
+import cn.hutool.core.lang.Dict;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.DescribeClusterResult;
-import org.apache.kafka.clients.admin.ListTopicsResult;
+import org.apache.kafka.clients.admin.TopicDescription;
+import org.apache.kafka.common.KafkaFuture;
 import org.apache.kafka.common.Node;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.AbstractHealthIndicator;
@@ -11,7 +13,9 @@ import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 @Component
 public class KafkaHealthIndicator extends AbstractHealthIndicator {
@@ -19,33 +23,58 @@ public class KafkaHealthIndicator extends AbstractHealthIndicator {
     private final KafkaAdmin kafkaAdmin;
     private final EasyKafkaConfig easyKafkaConfig;
 
-
     public KafkaHealthIndicator(KafkaAdmin kafkaAdmin, @Autowired(required = false) EasyKafkaConfig easyKafkaConfig) {
         this.kafkaAdmin = kafkaAdmin;
         this.easyKafkaConfig = easyKafkaConfig;
     }
 
     @Override
-    public void doHealthCheck(Health.Builder builder) {
+    protected void doHealthCheck(Health.Builder builder) {
         if (easyKafkaConfig == null || easyKafkaConfig.getTopic() == null) {
             builder.unknown().withDetail("error", "未启用kafka配置");
             return;
         }
-        try {
-            AdminClient adminClient = AdminClient.create(kafkaAdmin.getConfigurationProperties());
-            ListTopicsResult listTopicsResult = adminClient.listTopics();
-            Set<String> topicNames = listTopicsResult.names().get();
-            DescribeClusterResult clusterResult = adminClient.describeCluster();
-            Collection<Node> nodes = clusterResult.nodes().get();
-            final String clusterId = clusterResult.clusterId().get();
-            final Node controller = clusterResult.controller().get();
-            builder.up().withDetail("topics", topicNames)
-                    .withDetail("nodes", nodes)
-                    .withDetail("clusterId", clusterId)
-                    .withDetail("controller", controller);
+
+        try (AdminClient adminClient = AdminClient.create(kafkaAdmin.getConfigurationProperties())) {
+            builder.up();
+            addTopicDetails(adminClient, builder);
+            addClusterDetails(adminClient, builder);
+        } catch (InterruptedException e) {
+            builder.down().withException(e);
+            Thread.currentThread().interrupt();
         } catch (Exception e) {
-            builder.down().withDetail("error", e.getMessage());
+            builder.down().withException(e);
         }
     }
 
+    private void addTopicDetails(AdminClient adminClient, Health.Builder builder) throws ExecutionException, InterruptedException {
+        Set<String> topicNames = adminClient.listTopics().names().get();
+        Map<String, KafkaFuture<TopicDescription>> topicNameValues = adminClient.describeTopics(topicNames).topicNameValues();
+
+        for (KafkaFuture<TopicDescription> topic : topicNameValues.values()) {
+            TopicDescription topicDescription = topic.get();
+            Object[] array = topicDescription.partitions().stream().map(partitionInfo -> Dict.create()
+                    .set("partition", partitionInfo.partition())
+                    .set("leader", partitionInfo.leader().host())
+                    .set("replicas", partitionInfo.replicas().stream().map(Node::host))
+                    .set("inSyncReplicas", partitionInfo.isr().stream().map(Node::host))).toArray();
+
+            builder.withDetail(topicDescription.name(), Dict.create()
+                    .set("topic", topicDescription.name())
+                    .set("partitions", array)
+                    .set("internal", topicDescription.isInternal()));
+        }
+    }
+
+    private void addClusterDetails(AdminClient adminClient, Health.Builder builder) throws ExecutionException, InterruptedException {
+        DescribeClusterResult clusterResult = adminClient.describeCluster();
+        Collection<Node> nodes = clusterResult.nodes().get();
+        String clusterId = clusterResult.clusterId().get();
+        Node controller = clusterResult.controller().get();
+
+        builder.withDetail("topics", adminClient.listTopics().names().get())
+                .withDetail("nodes", nodes.stream().map(Node::host))
+                .withDetail("clusterId", clusterId)
+                .withDetail("controller", controller.host());
+    }
 }
