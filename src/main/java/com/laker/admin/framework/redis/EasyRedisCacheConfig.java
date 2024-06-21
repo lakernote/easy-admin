8de4e7281e76662426a60fbaa3423b48d5a56285
laker <935009066@qq.com>
2024-06-21 11:21:00 +0800

    feature: add redissoncachemetrics

--- src/main/java/com/laker/admin/framework/redis/EasyRedisCacheConfig.java
+++ src/main/java/com/laker/admin/framework/redis/EasyRedisCacheConfig.java
@@ -3,6 +3,7 @@ package com.laker.admin.framework.redis;
 import org.redisson.api.RedissonClient;
 import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.spring.cache.CacheConfig;
+import org.redisson.spring.cache.RedissonCacheMeterBinderProvider;
 import org.redisson.spring.cache.RedissonSpringCacheManager;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.cache.annotation.EnableCaching;
@@ -34,6 +35,11 @@ public class EasyRedisCacheConfig {
         return getRedissonSpringCacheManager(redissonClient, cacheConfigMap);
     }
 
+    @Bean
+    public RedissonCacheMeterBinderProvider redissonCacheMeterBinderProvider(){
+        return new RedissonCacheMeterBinderProvider();
+    }
+
     private static RedissonSpringCacheManager getRedissonSpringCacheManager(RedissonClient redissonClient, Map<String, CacheConfig> cacheConfigMap) {
         RedissonSpringCacheManager redissonSpringCacheManager = new RedissonSpringCacheManager(redissonClient);
         // 设置是否允许缓存的值为null。默认值为false，即缓存的值不允许为null。
@@ -41,6 +47,7 @@ public class EasyRedisCacheConfig {
         // 设置缓存是否感知Spring管理的事务。如果设置为true，则缓存操作只会在事务成功提交后的after-commit阶段执行，
         // 即在事务成功完成后才会执行put/evict操作。默认值为false，即缓存不感知Spring管理的事务。
         redissonSpringCacheManager.setTransactionAware(false);
+        // 设置缓存的配置信息
         redissonSpringCacheManager.setConfig(cacheConfigMap);
         // 设置缓存的编码方式
         redissonSpringCacheManager.setCodec(new JsonJacksonCodec());
