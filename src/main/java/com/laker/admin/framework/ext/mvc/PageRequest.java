4fdef36bc90b67c6af840381ee1555c445b5f00d
laker <935009066@qq.com>
2024-09-10 13:23:04 +0800

    refactor: code cleanup

--- src/main/java/com/laker/admin/framework/ext/mvc/PageRequest.java
+++ src/main/java/com/laker/admin/framework/ext/mvc/PageRequest.java
@@ -2,6 +2,7 @@ package com.laker.admin.framework.ext.mvc;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,15 +11,22 @@ import lombok.ToString;
 /**
  * 用于映射查询请求参数 分页、排序和过滤功能
  * ?page=1&size=2&sort=rank|asc,zip_code|desc&filter=city|eq|beijing
+ * 被 PageRequestArgumentResolver 解析
  */
 @Builder
 @ToString
 @Getter
+@Schema(description = "查询请求参数 分页、排序和过滤功能", example = "?page=1&size=2&sort=rank|asc,zip_code|desc&filter=city|eq|beijing")
 public class PageRequest {
     @Schema(description = "页码，从1开始，默认1")
     private int page;
     @Schema(description = "每页大小，默认10")
     private int size;
+
+    /**
+     * 用于在 JSON 序列化和反序列化过程中忽略某个属性。
+     */
+    @JsonIgnore
     private QueryWrapper queryWrapper;
 
     public Page toPage() {
