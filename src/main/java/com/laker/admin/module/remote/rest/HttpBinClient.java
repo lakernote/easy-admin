e63ce64bee5b3fc4e5011ad2510a08e47e9d3d70
laker <935009066@qq.com>
2024-11-05 17:15:41 +0800

    refactor: code cleanup

--- src/main/java/com/laker/admin/module/remote/rest/HttpBinClient.java
+++ src/main/java/com/laker/admin/module/remote/rest/HttpBinClient.java
@@ -32,6 +32,7 @@ public class HttpBinClient {
                 .DEFAULTS
                 .withConnectTimeout(Duration.ofSeconds(5))
                 .withReadTimeout(Duration.ofSeconds(10));
+        // 使用默认的 jdk 11 http 客户端 ,自带连接池
         ClientHttpRequestFactory requestFactory = ClientHttpRequestFactories.get(JdkClientHttpRequestFactory.class, settings);
         this.restClient = restClientBuilder
                 .baseUrl("https://httpbin.org/")
